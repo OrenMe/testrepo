640394dcd049ef8435f4647c731ef4aa24942dd6
Oren Me <oren.me@gmail.com>
2021-02-17 11:16:14 +0200

    feat: add webpack build (#28)

---
+++ src/index.js
@@ -0,0 +1,23 @@
+export const X = 1000;
+export const Y = 1000;
+export const Z = 1000;
+export const T = 1001;
+
+export function main(){
+	return false;
+}
+
+export function more(){
+	return 1;
+}
+
+export function evenMore(){
+}
+
+export function andEvenMore(){
+}
+
+export function andEvenMore2(){
+}
+export function andEvenMore3(){}
+export function andEvenMore4(){}
